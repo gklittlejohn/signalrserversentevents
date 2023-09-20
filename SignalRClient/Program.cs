b9a752e682ee3af0e0580a8abae5091ba781a231
graeme.littlejohn <graeme.littlejohn@hrhgeology.com>
2023-09-20 12:22:21 +0100

    Initial commit

---
+++ SignalRClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HubConnection connection = new HubConnectionBuilder()
+               .WithUrl("https://localhost:7255/chatHub", Microsoft.AspNetCore.Http.Connections.HttpTransportType.ServerSentEvents)
+               .Build();
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            await connection.StartAsync();
+
+            await connection.StopAsync();
+        }
+    }
+}
